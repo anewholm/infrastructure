24c983623c7a0ff8018888abc8b60a8c1fe3ebd2
anewholm <annesley_newholm@yahoo.it>
2023-06-02 15:55:46 +0100

    laravel-websockets

--- assets/js/acornassociated.websocket.js
+++ assets/js/acornassociated.websocket.js
@@ -1,93 +1,142 @@
-function acornassociated_onMessage(message) {
-    var event = JSON.parse(message.data);
+import Echo   from './laravel-echo.js';
+import Pusher from './pusher-js.js';
 
-    if (!event.name) {
-        if (window.console) console.error(this, message);
-    } else {
-        var contexts    = (event.contexts ? event.contexts : ['']),
-            namespace   = event.namespace || 'websocket',
-            attrBase    = namespace + '-on' + event.name, // websocket-oncalendar
-            eventName   = namespace + ':'   + event.name; // websocket:calendar
-        if (window.console) console.log(this, event);
+window.Pusher = Pusher;
+window.Echo   = new Echo({
+    broadcaster: 'pusher',
+    key:     'intranet', // import.meta.env.VITE_PUSHER_APP_KEY,
+    cluster: 'intranet', // import.meta.env.VITE_PUSHER_APP_CLUSTER,
+    wsHost: window.location.hostname,
+    wsPort: 8081,
+    forceTLS: false, // TODO: Enable TLS
+    disableStats: true,
+});
 
-        // Attribute processing
-        // e.g. @websocket-oncalendar-1-12-update='conversation: #c1-2'
-        for (var i in contexts) {
-            var context      = contexts[i], // 1-12, thursday
-                contextDash  = (context ? '-' + context : ''),
-                contextBase  = attrBase + contextDash,   // websocket-oncalendar-1-12
-                updateAttrC  = contextBase + '-update',  // websocket-oncalendar-1-12-update
-                requestAttrC = contextBase + '-request', // websocket-oncalendar-1-12-request
-                successAttrC = contextBase + '-success', // websocket-oncalendar-1-12-success
-                soundAttrC   = contextBase + '-sound';   // websocket-oncalendar-1-12-sound
-            $('[' + updateAttrC + ']').each(function(){
-                var request = $(this).attr(requestAttrC) || 'onWebSocket',
-                    update  = $(this).attr(updateAttrC),
-                    success = $(this).attr(successAttrC),
-                    sound   = $(this).attr(soundAttrC);
-                if (request) {
-                    // Let us indicate which context we are using exactly
-                    var eventContext = JSON.parse(JSON.stringify(event));
-                    eventContext.contexts = [context];
+let acornassociated_wsConnections = {};
+$('[websocket-listen]').each(function(){
+    if (!acornassociated_wsConnections[location]) {
+        let channel, event,
+            channelEvent = $(this).attr('websocket-listen').split(':');
 
-                    // Send the whole event through, with context
-                    // for the AJAX handler to consider 
-                    $(this).request(request, {
-                        data:    eventContext,
-                        //update:  JSON.parse(update), // See below
-                        success: function(response, result, jXHR){
-                            if (window.console) console.log(response);
-                            if (update) {
-                                // We do this update manually
-                                // for efficiency, and to work off same backend Object 
-                                var jUpdate = JSON.parse('{' + update.replace(/'/g, '"') + '}');
-                                for (var partial in jUpdate) {
-                                    var path    = jUpdate[partial];
-                                    // TODO: Should work off the path, not the partial
-                                    // The onSearch() & updateList() works off getId()
-                                    var content = response[partial] || response[path];
-                                    if (content) $(path).html(content);
-                                }
-                            }
-                            if (sound) {
-                                var audio = new Audio(sound);
-                                audio.play();
-                            }
-                            if (success) eval(success);
-                        },
-                    });
-                }
+        if (channelEvent.length == 1) {
+            channel = channelEvent[0];
+            window.Echo
+                .channel(channel)
+                .listenToAll(function(channelEvent, eventMessage) {
+                    acornassociated_onEvent(channel, channelEvent.substring(1), eventMessage);
+                });
+        } else {
+            channel = channelEvent[0];
+            event   = channelEvent[1];
+            event   = '.' + event; // TODO: Why?
+
+            window.Echo
+                .channel(channel)
+                .listen(event, function(eventMessage) {
+                    acornassociated_onEvent(channel, event.substring(1), eventMessage, eventObject);
+                });
+        }
+        acornassociated_wsConnections[location] = true;
+    }
+});
+
+function acornassociated_onEvent(channel, event, eventMessage) {
+    let eventObject = {};
+    if (eventMessage instanceof Object) {
+        // Extract the first and only object and its class name
+        let keys = Object.keys(eventMessage);
+        if (keys.length) {
+            let className = keys[0];
+            eventObject = eventMessage[className];
+            eventObject.className = className;
+        }
+    }
+    
+    let attributeBases = [],
+        eventCum       = '',
+        eventSplit     = event.split(/[^a-zA-Z0-9]+/g), // [event, updated]
+        contexts       = (eventObject.contexts ? eventObject.contexts : []),
+        websocketBase  = 'websocket-on',          // websocket-on
+        channelBase    = websocketBase + channel; // websocket-oncalendar
+    if (window.console) console.log(arguments);
+    
+    // @attributes are hierarchical
+    attributeBases.push(websocketBase + 'any'); // websocket-onany
+    attributeBases.push(channelBase);           // websocket-oncalendar
+    // websocket-oncalendar-event
+    // websocket-oncalendar-event-updated
+    for (let eventPart of eventSplit) {
+        if (eventPart) {
+            eventCum += '-' + eventPart;
+            attributeBases.push(channelBase + eventCum);
+            // Add context to the cumulated event specs
+            // websocket-oncalendar-event-1-12
+            // websocket-oncalendar-event-updated-1-12
+            // NOTE: contexts might unserialize as an object, not an array
+            for (let i in contexts) {
+                let context = contexts[i];
+                if (context)
+                    attributeBases.push(channelBase + eventCum + '-' + context);
+            }
+        }
+    }
+
+
+    // Attribute processing
+    // e.g. @websocket-oncalendar-*-update='conversation: #c1-2'
+    for (let attrBase of attributeBases) {
+        let context      = attrBase.replace(/^websocket-on/, '').split(/-/g),
+            updateAttrC  = attrBase + '-update',  // websocket-oncalendar-*-update
+            requestAttrC = attrBase + '-request', // websocket-oncalendar-*-request
+            successAttrC = attrBase + '-success', // websocket-oncalendar-*-success
+            soundAttrC   = attrBase + '-sound';   // websocket-oncalendar-*-sound
+        $('[' + updateAttrC + '],[' + requestAttrC + ']').each(function(){
+            let request = $(this).attr(requestAttrC) || 'onWebSocket',
+                update  = $(this).attr(updateAttrC),
+                success = $(this).attr(successAttrC),
+                sound   = $(this).attr(soundAttrC);
+
+            // Send the whole event through, with context
+            // for the AJAX handler to consider 
+            $(this).request(request, {
+                data: {event:eventObject, context:context},
+                //update:  JSON.parse(update), // See below
+                success: function(response, result, jXHR){
+                    if (window.console) console.log(response);
+                    if (update) {
+                        // We do this update manually
+                        // for efficiency, and to work off same backend Object 
+                        let jUpdate = JSON.parse('{' + update.replace(/'/g, '"') + '}');
+                        for (var partial in jUpdate) {
+                            let path    = jUpdate[partial];
+                            // TODO: Should work off the path, not the partial
+                            // The onSearch() & updateList() works off getId()
+                            let content = response[partial] || response[path];
+                            if (content) $(path).html(content);
+                        }
+                    }
+                    if (sound) {
+                        let audio = new Audio(sound);
+                        audio.play();
+                    }
+                    // TODO: SECURITY: XSS possibilities
+                    //if (success) eval(success);
+                },
             });
-        }
+        });
+    }
 
-        // Dirty write protection
-        // editing-eventpart-id-130
-        if (event && event.class && event.ID) {
-            var cssClassClass = event.class.replace(/.*\\/, '').toLowerCase();
-            var cssClass = 'editing-' + cssClassClass + '-id-' + event.ID;
-            $('.' + cssClass).addClass('dirty-read');
-        }
+    // Dirty write protection
+    // TODO: We need the class name for Dirty write protection
+    // editing-eventpart-id-130
+    if (eventObject && eventObject.className && eventObject.id) {
+        let cssClassClass = eventObject.className.replace(/.*\\/, '').toLowerCase();
+        let cssClass = 'editing-' + cssClassClass + '-id-' + eventObject.id;
+        $('.' + cssClass).addClass('dirty-read');
+    }
 
-        // Trigger a global event
-        $(document).trigger(jQuery.Event(eventName), event); // websocket:calendar
+    // Trigger a global event
+    for (let attrBase of attributeBases) {
+        $(document).trigger(jQuery.Event(attrBase), [event, eventObject]); 
     }
 }
-
-var acornassociated_wsConnections = {};
-function acornassociated_wsConnect() {
-    $('[websocket-listen]').each(function(){
-        var self      = this;
-        var location  = $(this).attr('websocket-listen') || document.location.host + ':8081';
-        if (!acornassociated_wsConnections[location]) {
-            acornassociated_wsConnections[location] = new WebSocket("ws://" + location);
-            acornassociated_wsConnections[location].onmessage = function(){
-                acornassociated_onMessage.apply(self, arguments);
-            };
-        }
-    });
-}
-
-$(document).ready(acornassociated_wsConnect);
-$(window).on('ajaxUpdateComplete', acornassociated_wsConnect);
-
-
