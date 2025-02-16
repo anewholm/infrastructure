a9c5f22cd05e28a66c1fbb4b3eb63328566fe37d
anewholm <annesley_newholm@yahoo.it>
2025-02-16 17:16:45 +0300

    'no_message'

--- partials/_datetime.php
+++ partials/_datetime.php
@@ -1,9 +1,38 @@
 <?php
-if ($value && $value instanceof \DateTime) {
-    $current = new \DateTime();
-    $currentYear = $current->format("Y");
-    $year = $value->format("Y");
-    $date = ($currentYear == $year ? $value->format("M-d") : $value->format("Y-M-d"));
+use System\Helpers\DateTime as DateTimeHelper;
+
+if ($value) {
+    if (! $value instanceof \DateTime) $value = new \DateTime($value);
+
+    $current       = new \DateTime();
+    $currentYear   = $current->format("Y");
+    $year          = $value->format("Y");
+    $isCurrentYear = ($currentYear == $year);
+
+    // Hover
+    $day       = $value->format("d");
+    $dayName   = $value->format("D");
+    $week      = $value->format("w");
+    $month     = $value->format("m");
+    $monthName = $value->format("M");
+    $timeTense = DateTimeHelper::timeTense($value);
+
+    $title     = implode(', ', array(
+        $timeTense, 
+        "Day: $dayName ($day)", 
+        "Week: $week", 
+        "Month: $monthName ($month)", 
+        "Year: $year"
+    ));
+
+    $date = ($isCurrentYear ? $value->format("M-d") : $value->format("Y-M-d"));
     $time = $value->format("H:i");
-    print("<ul class='nowrap'><li>$date</li><li>$time</li></ul>");
+    
+    print(<<<HTML
+        <ul title="$title" class="nowrap" data-toggle="tooltip">
+            <li><span class="hover-indicator">$date</span></li>
+            <li><span class="hover-indicator">$time</span></li>
+        </ul>
+HTML
+    );
 }
